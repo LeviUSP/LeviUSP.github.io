9909dbfed15ad430c70975525a81930084d5e689
Levi <levifernandes@usp.br>
2019-05-28 12:42:11 -0300

    aba funcionando com javascript

--- js/tabScript.js
+++ js/tabScript.js
@@ -1,87 +1,24 @@
-//$(document).ready(function(){
+// function () {
 //    $("#content div:nth-child(1)").show();
-  //  $(".abas li:first div").addClass("selected");       
-  //  $(".aba").click(function(){
-//        $(".aba").removeClass("selected");
-//        $(this).addClass("selected");
-//        var indice = $(this).parent().index();
-//        indice++;
-//        $("#content div").hide();
-//        $("#content div:nth-child("+indice+")").show();
-//    });      
-//    $(".aba").hover(
-//        function(){$(this).addClass("ativa")},
-//        function(){$(this).removeClass("ativa")}
-//    );              
-//});
-
-function openPage(pageName, elmnt, color) {
-    // Hide all elements with class="tabcontent" by default */
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-  
-    // Remove the background color of all tablinks/buttons
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].style.backgroundColor = "";
-    }
-  
-    // Show the specific tab content
-    document.getElementById(pageName).style.display = "block";
-  
-    // Add the specific color to the button used to open the tab content
+//    $(".abas li:first div").addClass("selected");       
     
-  }
-  
-  // Get the element with id="defaultOpen" and click on it
-  document.getElementById("defaultOpen").click();
+           
+// };
+document.addEventListener("DOMContentLoaded", function(event) {
+   document.getElementById("aba1").classList.add("selected");   
+   
+});
 
 
+function abaClicada (elem){
+  var abaAnterior = document.getElementsByClassName("selected")[0];
+  if(abaAnterior != null) abaAnterior.classList.remove("selected");
+  elem.classList.add("selected");
 
-function openPage(){
+  var idDoConteudoAnterior = "conteudo_" + abaAnterior.id;
+  document.getElementById(idDoConteudoAnterior).style.display = "none";
 
-document.getElementById("tab1").style.visibility = "visible";
-document.getElementById("tab2").style.visibility = "collapse";
-document.getElementById("tab3").style.visibility = "collapse";
-document.getElementById("tab4").style.visibility = "collapse";
-
+  var idDoConteudoAtual = "conteudo_" + elem.id;
+  document.getElementById(idDoConteudoAtual).style.display = "block";
 
 }
-
-function openPage2(){
-
-  document.getElementById("tab1").style.visibility = "collapse";
-  document.getElementById("tab2").style.visibility = "visible";
-  document.getElementById("tab3").style.visibility = "collapse";
-  document.getElementById("tab4").style.visibility = "collapse";
-  
-  
-  }
-
-  function openPage3(){
-
-    document.getElementById("tab1").style.visibility = "collapse";
-    document.getElementById("tab2").style.visibility = "collapse";
-    document.getElementById("tab3").style.visibility = "visible";
-    document.getElementById("tab4").style.visibility = "collapse";
-    
-    
-    }
-    function openPage4(){
-
-      document.getElementById("tab1").style.visibility = "collapse";
-      document.getElementById("tab2").style.visibility = "collapse";
-      document.getElementById("tab3").style.visibility = "collapse";
-      document.getElementById("tab4").style.visibility = "visible";
-      
-      
-      }
-        
-    
-  
-
-
-
