6f12a3c781883230b240599c0406237e87cb0996
Levi <levifernandes@usp.br>
2019-05-28 18:21:32 -0300

    reset da pagina de abas

--- js/tabScript.js
+++ js/tabScript.js
@@ -1,14 +1,26 @@
-// function () {
-//    $("#content div:nth-child(1)").show();
-//    $(".abas li:first div").addClass("selected");       
-    
-           
-// };
 document.addEventListener("DOMContentLoaded", function(event) {
-   document.getElementById("aba1").classList.add("selected");   
+   document.getElementById("aba1").classList.add("selected");
+   
+   var conteudos = document.getElementsByClassName("tab_content");
+   
+   for (i = 0; i < conteudos.length; i++){
+	   conteudos[i].style.display = "none";
+   }
+
+   document.getElementById("conteudo_aba1").style.display = "block";
    
 });
 
+
+
+function abrepaginanaabacerta(esteElemento){
+  var idDaAba = "aba" + esteElemento.numeroDeID;
+  // vai pra pagina "avaliaçao"
+  document.getElementById(idDaAba).classList.add("selected"); 
+
+  document.getElementById("conteudo_"+idDaAba).style.display = "block";
+
+}
 
 function abaClicada (elem){
   var abaAnterior = document.getElementsByClassName("selected")[0];
