fe97c3d86eed1d1db540eb496b45d3c240f899a9
Levi <levifernandes@usp.br>
2019-05-30 10:38:21 -0300

    Funcionalidade de abrir na aba certa funcionando

--- js/tabScript.js
+++ js/tabScript.js
@@ -1,26 +1,22 @@
-var abaInicial = "aba1";
-
 document.addEventListener("DOMContentLoaded", function(event) {
-   document.getElementById(abaInicial).classList.add("selected");
+	abaInicial = localStorage.getItem('abaInicial')
+  
+  	document.getElementById(abaInicial).classList.add("selected");
    
-   var conteudos = document.getElementsByClassName("tab_content");
-   
-   for (i = 0; i < conteudos.length; i++){
-	   conteudos[i].style.display = "none";
-   }
+	var conteudos = document.getElementsByClassName("tab_content");
+	
+	for (i = 0; i < conteudos.length; i++){
+		conteudos[i].style.display = "none";
+	}
 
-   document.getElementById("conteudo_" + abaInicial).style.display = "block";
-   
+	document.getElementById("conteudo_" + abaInicial).style.display = "block";
+	
 });
 
 
 
 function abrePaginaNaAbaCerta(esteElemento){
-  abaInicial = "aba" + esteElemento.id;
-  // // vai pra pagina "avaliaçao"
-  // document.getElementById(idDaAba).classList.add("selected"); 
-
-  // document.getElementById("conteudo_"+idDaAba).style.display = "block";
+  localStorage.setItem('abaInicial', "aba" + esteElemento);
 
 }
 
